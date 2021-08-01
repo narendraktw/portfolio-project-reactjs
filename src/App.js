3a5d27fd1a6d6e4a357ab309acade22bc9fb010b
NarendraBishtPureSoftware <narendra.bisht@puresoftware.com>
2021-08-01 16:42:52 +0530

    services

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import Services from "./components/Services";
 function App() {
   return (
     <>
@@ -31,6 +32,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Services />
     </>
   );
 }
