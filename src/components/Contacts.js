eca89cafa17bbf6be2de6f7fff5cf695336c8275
NarendraBishtPureSoftware <narendra.bisht@puresoftware.com>
2021-08-02 00:03:13 +0530

    responsive fixes

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -8,7 +8,7 @@ const Contacts = () => {
 
   const serviceID = "service_ID";
   const templateID = "template_ID";
-  const userID = "user_RXtQk9xbkcuxeI8Wr9FD4";
+  const userID = "user_uQAxP0RQUMeHFtkCpiDw7";
 
   const onSubmit = (data, r) => {
     sendEmail(
