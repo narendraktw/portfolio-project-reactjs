7f9d982ece9fef924edd9e3c9b728706e528ec78
NarendraBishtPureSoftware <narendra.bisht@puresoftware.com>
2021-08-01 16:15:12 +0530

    about me

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 function App() {
   return (
     <>
@@ -29,6 +30,7 @@ function App() {
       />
       <Navbar />
       <Header />
+      <AboutMe />
     </>
   );
 }
