d3a92ca56283ad91cc615a8e30ef44bb9f6b9df7
narendraktw <narendraktw@gmail.com>
2021-08-30 13:31:35 +0530

    test

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -27,12 +27,28 @@ const Portfolio = () => {
       startDate: "2020",
       description:
         "The purpose of the Configuration Management Tool (CMT) is to simplify the process of gathering business requirements, configuring, and implementing those requirements into individual MHK Care Prominence client solutions. The tool will allow MHK Care Prominence product team to define and build Solution specific, out of the Box (OOB) configuration settings, along with storing previous versions. The CMT Tool will allow access to MHK Care Prominence clients to enter their specific business requirements and make changes to the predefined settings that align with their business needs",
+      technology: "ReactJs, Redux, ES6, HTML5, Material UI, Sass",
       demoLink: "",
       githubLink: "",
       client: "MHK CareProminence",
       images: [
         "https://raw.githubusercontent.com/narendraktw/portfolio-project-reactjs/main/src/assets/images/project/CMT/img1.jpg",
         "https://raw.githubusercontent.com/narendraktw/portfolio-project-reactjs/main/src/assets/images/project/LAT/img2.jpg",
+      ],
+    },
+    {
+      name: "Learning Plan for Every Seller",
+      startDate: "2019",
+      description:
+        "This site is designed to check the Honeywell sales, customer marketing, and offering management team’s performance. It provides development actions against competencies, learning plan, progress against development actions for managers and employees in one place. Accessed on mobile and desktop both devices.",
+      technology:
+        "Asp.net, ReactJs, Redux, JEST, Enzyme, ES6, HTML5, Bootstrap, AWS SQL Database, Tableau 10.3, Wordsmith",
+      demoLink: "",
+      githubLink: "",
+      client: "MHK CareProminence",
+      images: [
+        "https://raw.githubusercontent.com/narendraktw/portfolio-project-reactjs/main/src/assets/images/project/LPFES/img1.jpg",
+        "https://raw.githubusercontent.com/narendraktw/portfolio-project-reactjs/main/src/assets/images/project/LPFES/img2.jpg",
       ],
     },
   ];
