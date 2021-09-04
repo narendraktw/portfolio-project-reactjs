f933326d4ddf02b225557e3c1567dc31926e0010
narendraktw <narendraktw@gmail.com>
2021-09-04 23:01:01 +0530

    about me

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,11 +14,36 @@ const Experience = () => {
             <p>
               <h4>Associate Architect</h4>
               <h5>Puresoftware Pvt. Ltd</h5>
-              Developed MHK Care Prominence Configuration Management and Letter
-              Automation tool for MHK client.
+              <b>Responsibilities:</b>
+              <ul>
+                <li>Lead front-end code reviews with teammates.</li>
+                <li>
+                  Run reviews to guarantee the structure and design of the
+                  applications follow the standards and guidelines.
+                </li>
+                <li>
+                  Optimize logic-less templates using Front-end Template Engines
+                  and optimize JavaScript implementations.
+                </li>
+                <li>
+                  Generate documentation about UI architecture, interactions and
+                  behaviors with REST APIs or any other mechanism to process
+                  data.
+                </li>
+                <li>
+                  Document and assist in defining the functional and technical
+                  requirements of different applications.
+                </li>
+                <li>
+                  Implement front-end development workflows: building systems,
+                  dependency management, quality checkers (linting).
+                </li>
+              </ul>
+              <b>Projects:</b> MHK Care Prominence Configuration Management and
+              Letter Automation tool for MHK client.
               <br />
-              <b>Technology:</b> ReactJs, Redux, ES6, HTML5, Material UI, Sass,
-              MySQL Database, AWS S3 bucket.
+              <b>Technologies/Skills:</b> ReactJs, Redux, ES6, HTML5, Material
+              UI, Sass, MySQL Database, AWS S3 bucket.
             </p>
           </div>
         </div>
@@ -26,82 +51,71 @@ const Experience = () => {
         <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2019-2020</h3>
+            <h3>2011-2020</h3>
             <p>
               <h4>Lead Learning Developer</h4>
               <h5>Honeywell International Pvt. Ltd</h5>
-              Delegating work and assignments to team members. Collaborating
-              with their team to identify and fix technical problems. Developed
-              Learning Plan website for Sales, Customer Marketing & Offering
-              Management teams.
+              <b>Responsibilities:</b>
+              <ul>
+                <li>
+                  Manages software development life cycle starting from the
+                  phase of requirements analysis, design, implementation,
+                  integration, and testing.
+                </li>
+                <li>
+                  Communicates with business partners, project managers, UX
+                  teams, and team members to clearly define scope of work and
+                  provide accurate schedule estimates
+                </li>
+                <li>
+                  Lead front-end team and delegating work and assignments to
+                  team members.
+                </li>
+                <li>
+                  Collaborating with their team to identify and fix technical
+                  problems.
+                </li>
+                <li>
+                  Run reviews to guarantee the structure and design of the
+                  applications follow the standards and guidelines.
+                </li>
+                <li>
+                  Optimize logic-less templates using Front-end Template Engines
+                  and optimize JavaScript implementations.
+                </li>
+              </ul>
+              <b>Projects:</b> Learning Plan website for Sales, Customer
+              Marketing & Offering Management teams. Quota Deployment tool,
+              Price Deviation Tool, Indirect Spends Tool, Digital WorkSpace(DWP)
+              & My Webtech Portal.
               <br />
-              <b>Technology:</b> Asp.net, ReactJs, Redux, JEST, Enzyme, ES6,
-              HTML5, Bootstrap, AWS SQL Database, Tableau 10.3, Wordsmith.
+              <b>Technologies/Skills:</b> Asp.net, ReactJs, Redux, JEST, Enzyme,
+              ES6, HTML5, Bootstrap, Sharepoint, Nintex Workflow, AWS SQL
+              Database, Tableau 10.3,P Services, Microsoft Flows, JavaScript,
+              jQuery Wordsmith.
             </p>
           </div>
         </div>
+
         {/* - */}
         <div className="timeline-block timeline-block-right">
-          <div className="marker"></div>
-          <div className="timeline-content">
-            <h3>2017-2019</h3>
-            <p>
-              <h4>Senior Web Programmer</h4>
-              <h5>Honeywell International Pvt. Ltd</h5>
-              Developed Quota deveployment tool and Learning portal for
-              Honeywell Process Solution business and Corporate team.
-              <br />
-              <b>Technology:</b>: ReactJs, Asp.net, ES6, HTML5, Bootstrap, SQL
-              Server.
-            </p>
-          </div>
-        </div>
-        {/* - */}
-        <div className="timeline-block timeline-block-left">
-          <div className="marker"></div>
-          <div className="timeline-content">
-            <h3>2014-2017</h3>
-            <p>
-              <h4>Web Developer Specialist</h4>
-              <h5>Honeywell International Pvt. Ltd</h5>
-              Developed and migrated Price Deviation, Indirect Spends and
-              Digital Workspace tool for Honeywell Safety Products & Honeywell
-              internal team.
-              <br />
-              <b>Technology:</b> SharePoint 2013/Online, Nintex Workflow,
-              Agilepoint workflow, SP Services, JavaScript, jQuery, HTML5, CSS3,
-              Bootstrap.
-            </p>
-          </div>
-        </div>
-        {/* - */}
-        <div className="timeline-block timeline-block-right">
-          <div className="marker"></div>
-          <div className="timeline-content">
-            <h3>2011-2014</h3>
-            <p>
-              <h4>Web Developer Associates</h4>
-              <h5>Honeywell International Pvt. Ltd</h5>
-              Developed Project Management, Mywebtech and Product Creation
-              Request tool for Honeywell Home Security & Honeywell Safety
-              Products
-              <br />
-              <b>Technology:</b> SharePoint 2010/2013/Online, Nintex Workflow,
-              SP Services, Microsoft Flows, JavaScript, jQuery, HTML5, CSS3.
-            </p>
-          </div>
-        </div>
-        {/* - */}
-        <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
             <h3>2010-2011</h3>
             <p>
               <h4>WebMaster</h4>
               <h5>Manpower Pvt. Ltd</h5>
-              Design and Maintain websites HoneywellCity and HSMCity for
-              Honeywell different partners. <br />
-              <b>Technology:</b> Asp.net, JavaScript, jQuery, HTML5, CSS3.
+              <b>Responsibilities:</b>
+              <ul>
+                <li>Design and Maintain websites</li>
+                <li>
+                  Coordinate with development team and provide support to all
+                  web development
+                </li>
+              </ul>
+              <b>Projects:</b> HSMCity for Honeywell different partners. <br />
+              <b>Technologies/Skills:</b> Asp.net, JavaScript, jQuery, HTML5,
+              CSS3.
             </p>
           </div>
         </div>
