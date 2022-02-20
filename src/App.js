749226c30fd8e064582ea268aacc47d1c69e87e5
narendraktw <narendraktw@gmail.com>
2022-02-20 12:45:28 +0530

    update in about me and enable services

--- src/App.js
+++ src/App.js
@@ -4,47 +4,47 @@ import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
-// import Services from "./components/Services";
+import Services from "./components/Services";
 import Experience from "./components/Experience";
 import Portfolio from "./components/Portfolio";
 import Testimonials from "./components/Testimonials";
 import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
 function App() {
-  return (
-    <>
-      <Particles
-        className="particles-canvas"
-        params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
-                enable: true,
-                value_area: 1000,
-              },
-            },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 6,
-                color: "#f9ab00",
-              },
-            },
-          },
-        }}
-      />
-      <Navbar />
-      <Header />
-      <AboutMe />
-      {/* <Services /> */}
-      <Experience />
-      <Portfolio />
-      <Testimonials />
-      <Contacts />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Particles
+				className="particles-canvas"
+				params={{
+					particles: {
+						number: {
+							value: 30,
+							density: {
+								enable: true,
+								value_area: 1000,
+							},
+						},
+						shape: {
+							type: "circle",
+							stroke: {
+								width: 6,
+								color: "#f9ab00",
+							},
+						},
+					},
+				}}
+			/>
+			<Navbar />
+			<Header />
+			<AboutMe />
+			<Services />
+			<Experience />
+			<Portfolio />
+			<Testimonials />
+			<Contacts />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,40 +1,41 @@
 import React from "react";
 import author from "../assets/images/myPic.png";
 const AboutMe = () => {
-  return (
-    <div id="about" className="aboutMe ptb">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-6 col-xm-12">
-            <div className="photo-wrap mb-5">
-              <img className="profile-img" src={author} alt="author..." />
-            </div>
-          </div>
-          <div className="col-lg-6 col-xm-12">
-            <h1 className="about-heading">about me</h1>
-            <p>
-              Hello! I am Narendra Bisht. Result-driven Full Stack Software
-              Developer with 10 years of extensive experience in developing web
-              applications and cross-platform mobile apps using cutting-edge
-              open-source libraries and frameworks. Proficient in spearheading
-              full software development life cycle – from idea through the
-              delivery of next-generation applications and customized solutions.
-              Versatile experience in start-up products to Enterprise
-              applications. Proven ability to lead technical teams to produce
-              high-quality deliverables that meet or exceed timeline and
-              budgetary targets. A strategic thinker with superior verbal &
-              written communications skills.
-              <br />
-              <b>Technologies/Skills:</b> ReactJs, Redux, Javascript,
-              NodeJs(Rest APIs), ASP.net, ES6+, HTML5, Material UI, Sass, CSS3,
-              Bootstrap, Sharepoint, Nintex Workflow, Tableau, SQL Server &
-              MongoDB.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="about" className="aboutMe ptb">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-6 col-xm-12">
+						<div className="photo-wrap mb-5">
+							<img className="profile-img" src={author} alt="author..." />
+						</div>
+					</div>
+					<div className="col-lg-6 col-xm-12">
+						<h1 className="about-heading">about me</h1>
+						<p>
+							Hello! I am Narendra Bisht. Result-driven Full Stack Software
+							Developer with 10+ years of extensive experience in developing web
+							applications and cross-platform mobile apps using cutting-edge
+							open-source libraries and frameworks. Proficient in spearheading
+							full software development life cycle – from idea through the
+							delivery of next-generation applications and customized solutions.
+							Versatile experience in start-up products to Enterprise
+							applications, Existing bussiness process improvement, and
+							Automation. Proven ability to lead technical teams to produce
+							high-quality deliverables that meet or exceed timeline and
+							budgetary targets. A strategic thinker with superior verbal &
+							written communications skills.
+							<br />
+							<b>Technologies/Skills:</b> ReactJs, Redux, JavaScript,
+							NodeJs(Rest APIs), ASP.net, ES6+, HTML5, Material UI, Sass, CSS3,
+							Bootstrap, AWS Cloud, Sharepoint Online, Nintex Workflow, Tableau,
+							SQL Server & MongoDB.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AboutMe;
